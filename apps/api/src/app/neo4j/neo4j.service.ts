5943b22146c1771138173149d270d692971dbfb9
Wessel Kuijstermans <w.kuijstermans@home.nl>
2023-02-10 14:59:59 +0100

    added follow relationship in neo

--- apps/api/src/app/neo4j/neo4j.service.ts
+++ apps/api/src/app/neo4j/neo4j.service.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import { Injectable } from '@nestjs/common';
 require('dotenv').config();
-import neo4j, { Node, Relationship } from 'neo4j-driver';
+import neo4j from 'neo4j-driver';
 
 @Injectable()
 export class Neo4jService {
@@ -10,19 +10,19 @@ export class Neo4jService {
     neo4j.auth.basic('neo4j', process.env.NEO_PASSWORD)
   );
 
-  async test() {
-    const session = this.driver.session();
+  // async test() {
+  //   const session = this.driver.session();
 
-    try {
-        const res = await session.executeWrite(req => req.run(`MERGE (p:Person {name: $person})-[:OWNS]->(mon:Pokemon{name: $pokemon}) RETURN p.name as p, mon.name as mon`, {person: 'Wessel', pokemon: 'bulbasaur'}));
-        const person = res.records.map(row => row.get('p'));
-        const pokemon = res.records.map(row => row.get('mon'));
-        const results = {person, pokemon}
-        return results;
-    } finally {
-        session.close();
-    }
-  }
+  //   try {
+  //       const res = await session.executeWrite(req => req.run(`MERGE (p:Person {name: $person})-[:OWNS]->(mon:Pokemon{name: $pokemon}) RETURN p.name as p, mon.name as mon`, {person: 'Wessel', pokemon: 'bulbasaur'}));
+  //       const person = res.records.map(row => row.get('p'));
+  //       const pokemon = res.records.map(row => row.get('mon'));
+  //       const results = {person, pokemon}
+  //       return results;
+  //   } finally {
+  //       session.close();
+  //   }
+  // }
 
   async addUser(username: string) {
     const session = this.driver.session();
@@ -51,9 +51,20 @@ export class Neo4jService {
 
     try {
       const res = await session.executeWrite(req => req.run(`MERGE (p:Person {name: $person}) MERGE (mon:Pokemon {name: $pokemon}) MERGE (p)-[:OWNS]->(mon)`, {person, pokemon}));
-      return res.records.map(row => row.get('p'))
+      return res.records.map(row => row.get('p'));
     } finally {
-      session.close()
+      session.close();
+    }
+  }
+
+  async follow(p1: string, p2:string) {
+    const session = this.driver.session();
+
+    try {
+      const res = await session.executeWrite(req => req.run(`MERGE (p1:Person {name: $p1}) MERGE (p2:Person {name: $p2}) MERGE (p1)-[:FOLLOWS]->(p2)`, {p1, p2}));
+      return res.records.map(row => row.get('p'));
+    } finally {
+      session.close();
     }
   }
 }
